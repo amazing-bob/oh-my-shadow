c2f900dd9bd3a87ac1698f3cea1e9272c3a06786
Sangheon <buru1020@gmail.com>
2023-08-28 21:44:13 +0900

    - print 함수 추가

--- src/test/java/io/amazingbob/file/ParameterConverterTest.java
+++ src/test/java/io/amazingbob/file/ParameterConverterTest.java
@@ -61,51 +61,7 @@ class ParameterConverterTest {
         assertThrows(FileNotFoundException.class, () -> new ParameterConverter(targetFile).convert("empty", convertFileName));
         assertEquals(convertFileFolder + "/" + targetFileName, new ParameterConverter(targetFile).convert(convertFileFolder, null).getPath());
         assertEquals(targetFileFolder + "/" + targetFileName, new ParameterConverter(targetFile).convert(targetFileFolder, targetFileName).getPath());
-
     }
-
-
-
-//    @BeforeEach
-//    void setUp () {
-//        File tobeFolder = new File("/Users/buru/Projects/oh-my-shadow/src/main/resources/test-data/replace/to-be");
-//        for ( File file : tobeFolder.listFiles() ) {
-//            file.delete();
-//            file.renameTo
-//        }
-//
-//        originFolderPath = "/Users/buru/Projects/oh-my-shadow/src/main/resources/test-data/replace/as-is";
-//        originFileName = "SampleController.java";
-//        originFile = new File(originFolderPath, originFileName);
-//
-//        convertedFolderPath = "/Users/buru/Projects/oh-my-shadow/src/main/resources/test-data/replace/to-be";
-//        convertedFileName = "SampleController.java";
-//        convertedFile = new File(convertedFolderPath, convertedFileName);
-//
-//        converterS = new ParameterConverter(
-//                new FileInfo(originFolderPath, originFileName),
-//                new FileInfo(convertedFolderPath, convertedFileName));
-//
-//        converterF = new ParameterConverter(
-//                new FileInfo(originFolderPath, originFileName),
-//                new FileInfo(convertedFolderPath, convertedFileName));
-//
-//    }
-//
-//    @DisplayName("1. converterInit")
-//    @Test
-//    void isInitConverter() throws IOException {
-//        assertEquals(originFolderPath, converterS.getOriginFileInfo().getFolderPath());
-//        assertEquals(originFileName, converterS.getOriginFileInfo().getFileName());
-//        assertEquals(originFile, converterS.getOriginFileInfo().getFile());
-//        assertEquals(8, converterS.getConvertedMethodCount());
-//
-//        assertEquals(originFolderPath, converterF.getOriginFileInfo().getFolderPath());
-//        assertEquals(originFileName, converterF.getOriginFileInfo().getFileName());
-//        assertEquals(originFile, converterF.getOriginFileInfo().getFile());
-//        assertEquals(8, converterF.getConvertedMethodCount());
-//
-//    }
 
 
 }
